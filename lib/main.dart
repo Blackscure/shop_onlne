e26eab845b8b11732c75f30f1fb48885b89f4c95
blackscure <Wekesabuyahi@gmail.com>
2021-04-03 01:08:32 +0300

    login,home screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:html';
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shop/screens/landing_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,43 +11,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LandingPage());
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: LandingPage());
   }
 }
-
-class LandingPage extends StatelessWidget {
-  final Future<FirebaseApp> _Initialization = Firebase.initializeApp();
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _Initialization,
-      builder: (context, snapshot) {
-        // if snapshot has error
-        if (snapshot.hasError) {
-          return Scaffold(
-            body: Center(
-              child: Text("Error: ${snapshot.error}"),
-            ),
-          );
-        }
-
-        // Connection initialized - Firebase App is running
-        if (snapshot.connectionState == ConnectionState.done) {
-          Scaffold(
-            body: Center(
-              child: Text("Firebase App Initialized"),
-            ),
-          );
-        }
-
-        // connecting to firebase
-
-        return Scaffold(
-          body: Center(
-            child: Text("initializing App..."),
-          ),
-        );
-      },
-    );
-  }
-}
+ 
